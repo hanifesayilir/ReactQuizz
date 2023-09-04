6e6a9c7d73dba6f79dda5c8568c3fe58ecaefc3c
Hanife <hanife.sayilir@gmail.com>
2023-09-04 08:39:08 +0100

    update

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -4,13 +4,18 @@ import {useEffect, useReducer} from "react";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartsScreen from "./StartsScreen";
+import Question from "./Question";
+import NextButton from "./components/NextButton";
 
 
 const initialState = {
     questions: [],
 
     // 'loading','error', 'active','finished','ready'
-    status: 'loading'
+    status: 'loading',
+    index: 0,
+    answer: null,
+    points: 0,
 };
 
 function reducer(state,action){
@@ -19,9 +24,22 @@ function reducer(state,action){
         case "dataReceived": return { ...state,
             questions: action.payload,
             status: "ready"
-        }
+        };
         case "dataFailed": return { ...state,
-        status: "error"}
+        status: "error"};
+        case "start": return {...state,
+        status: "active"};
+
+        case "newAnswer":
+            const question = state.questions.at(state.index);
+            return {...state,
+            answer: action.payload,
+            points: action.payload === question.correctOption ? state.points +question.points : state.points,
+        };
+        case "nextQuestion": return {...state,
+        index: state.index+1, answer: null
+        }
+
         default:
             throw new Error("Action Unknown")
     }
@@ -30,9 +48,9 @@ function reducer(state,action){
 
 export default function App () {
 
-    const [{questions, status},dispatch] = useReducer(reducer,initialState);
+    const [{questions, status, index,answer},dispatch] = useReducer(reducer,initialState);
 
-    const num
+    const numberOfQuestions = questions.length;
 
     useEffect(()=>{
         fetch("http://localhost:8000/questions")
@@ -48,7 +66,22 @@ export default function App () {
           <Main />
           {status === 'loading' && <Loader />}
           {status === 'error' && <Error />}
-          {status === 'ready' && <StartsScreen />}
+          {status === 'ready' && <StartsScreen numberOfQuestions={numberOfQuestions} dispatch={dispatch}/>}
+          {status === 'active' && (
+              <>
+
+                  <Question
+                      question={questions[index]}
+                      dispatch={dispatch}
+                      answer={answer}
+                  />
+                  <NextButton dispatch={dispatch} answer={answer}/>
+              </>
+              )
+
+
+
+          }
       </div>
       )
 
--- react-quiz/src/StartsScreen.js
+++ react-quiz/src/StartsScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function StartsScreen(props) {
+function StartsScreen({numberOfQuestions, dispatch}) {
     return (
         <div className="start">
             <h2>Welcome to the React Quizz</h2>
-            <h3>x question to test your React mastery</h3>
-            <button>Let's start</button>
+            <h3>{numberOfQuestions} questions to test your React mastery</h3>
+            <button className="btn btn-ui"
+                    onClick={()=>dispatch({type: "start"})}
+            >Let's start
+            </button>
         </div>
     );
 }
